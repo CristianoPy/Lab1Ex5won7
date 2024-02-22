0d8692b91eb8a85da81bb650be93c7864d9e2e1e
Cristiano Py <Cristiano Py@DESKTOP-U2EFTL3>
2024-02-22 10:49:35 +0200

    Add project files.

---
+++ Lab1Ex5won7/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele
+doua valori in ordine crescatoare.
+• Exemplu: citim 9,0 Afisam : 0 9*/
+
+
+Console.WriteLine("Introduce primul numar: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introduce a doilea numar: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Numere in ordine crescatore sunt: ");
+
+if (x > y)
+{
+    if (y > x)
+        Console.WriteLine($"{x}, {y} ");
+
+    else if (x > y)
+        Console.WriteLine($"{y}, {x}");
+
+    else
+        Console.WriteLine($"{x}, {y}");
+    
+}
+else if (y > x)
+{
+    if (y > x)
+    {
+        if (y > x)
+            Console.WriteLine($"{x}, {y} ");
+
+        else
+            Console.WriteLine($"{x}, {y}");
+    }
+}
+
+Console.ReadLine();
+
+
